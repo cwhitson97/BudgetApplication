9fa1a9f65347935be46b39e8b048e2d5af74174b
Courtney Whitson <whitsoncourtney97@gmail.com>
2021-01-06 00:12:21 -0600

    Start of Menu

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from kivy.core.text import LabelBase
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-# Loads kv config files
+# Loads config files
 Builder.load_file('Login.kv')
+Builder.load_file('menu.kv')
 Config.set('graphics', 'fullscreen', '0')
 
 # Creates an object to manage multiple kv screens
@@ -21,8 +22,13 @@ class LoginScreen(Screen):
     pass
 
 
+class MenuScreen(Screen):
+    pass
+
+
 # Adds screens to the screen manager
 sm.add_widget(LoginScreen(name='login'))
+sm.add_widget(MenuScreen(name='menu'))
 
 
 class LivingFree(App):
